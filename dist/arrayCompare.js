e33cb2ffa370e0debb3d00cc4b44ab601282a856
Oren (electricessence) <oren@electrified.net>
2020-05-12 19:23:21 -0700

    Refresh.

--- dist/arrayCompare.js
+++ dist/arrayCompare.js
@@ -4,6 +4,7 @@
  * Licensing: MIT
  */
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.areEquivalent = exports.areEqual = exports.areAllEqual = void 0;
 const tslib_1 = require("tslib");
 const areEqual_1 = tslib_1.__importDefault(require("@tsdotnet/compare/dist/areEqual"));
 const ArgumentNullException_1 = tslib_1.__importDefault(require("@tsdotnet/exceptions/dist/ArgumentNullException"));
